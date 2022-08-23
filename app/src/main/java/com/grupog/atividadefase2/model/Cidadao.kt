b66779f0973abe96c89f6c78d47a24dff123a329
Ana Sofia <nunez.asofia@gmail.com>
2022-08-22 23:29:24 -0300

    Criação do arquivo DataBase.kt para criação de bancos de dados locais incluindo duas tabelas: tbl_cidadao e tbl_imoveis Criação das classes Imovel.kt e Cidadao.kt e suas respectivas persistências ImovelDAO.kt e CidadaoDAO.kt Inclusão de uma função para gravar usuário ao clicar no botão de login da MainActivity.kt

---
+++ app/src/main/java/com/grupog/atividadefase2/model/Cidadao.kt
@@ -0,0 +1,8 @@
+package com.grupog.atividadefase2.model
+
+data class Cidadao (
+    var id_cidadao: Int = 0,
+    var nome: String = "",
+    var cpf: String = "",
+    var telefone: String = "",
+    )
